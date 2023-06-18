7f99ac19d951a86836bc347b4d809e8f519a564a
Abhisek Kumar Gupta <codewhispererisalive@gmail.com>
2023-06-19 01:03:35 +0530

    add factory constructor that takes a Map<String, dynamic> and creates a new PetCard instance by extracting the values from the map

--- lib/models/per_card.dart
+++ lib/models/per_card.dart
@@ -1,15 +1,39 @@
-class PetCard {
+class Pet {
   final String imageAsset;
   final String name;
   final int age;
+  final int price;
   final String breed;
   final String type;
 
-  PetCard({
+  Pet({
     required this.imageAsset,
     required this.name,
     required this.age,
+    required this.price,
     required this.breed,
     required this.type,
   });
+
+  factory Pet.fromJson(Map<String, dynamic> json) {
+    return Pet(
+      imageAsset: json['asset'] ?? '',
+      name: json['name'] ?? '',
+      age: json['age'] ?? 0,
+      price: json['price'] ?? 0,
+      breed: json['breed'] ?? '',
+      type: json['type'] ?? '',
+    );
+  }
+
+  Map<String, dynamic> toJson() {
+    return {
+      'asset': imageAsset,
+      'name': name,
+      'age': age,
+      'price': price,
+      'breed': breed,
+      'type': type,
+    };
+  }
 }
