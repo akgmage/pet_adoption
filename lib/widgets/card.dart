b28f1e0f0f984ed4bd4603c4725c8bc9267440f4
Abhisek Kumar Gupta <codewhispererisalive@gmail.com>
2023-06-19 23:29:38 +0530

    remove hardcode color and boxshadow

--- lib/widgets/card.dart
+++ lib/widgets/card.dart
@@ -14,72 +14,65 @@ class PetCard extends StatelessWidget {
       padding: const EdgeInsets.symmetric(horizontal: Sizes.containerPadding),
       child: Container(
         decoration: BoxDecoration(
-          color: Colors.white,
           borderRadius: BorderRadius.circular(Sizes.borderRadius),
-          boxShadow: [
-            BoxShadow(
-              color: Colors.grey.withOpacity(0.3),
-              spreadRadius: Sizes.spreadRadius,
-              blurRadius: Sizes.blurRadius,
-              offset: const Offset(0, 3),
-            ),
-          ],
         ),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            ClipRRect(
-              borderRadius: const BorderRadius.vertical(
-                  top: Radius.circular(Sizes.borderRadius)),
-              child: Image.asset(
-                pet.imageAsset,
-                height: Sizes.cardImageHeight,
-                width: double.infinity,
-                fit: BoxFit.fill,
+        child: Card(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              ClipRRect(
+                borderRadius: const BorderRadius.vertical(
+                    top: Radius.circular(Sizes.borderRadius)),
+                child: Image.asset(
+                  pet.imageAsset,
+                  height: Sizes.cardImageHeight,
+                  width: double.infinity,
+                  fit: BoxFit.fill,
+                ),
               ),
-            ),
-            Padding(
-              padding: const EdgeInsets.all(Sizes.cardDetailPadding),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Text(
-                    pet.name,
-                    style: const TextStyle(
-                      fontWeight: FontWeight.bold,
-                      fontSize: Sizes.normalFontSize,
+              Padding(
+                padding: const EdgeInsets.all(Sizes.cardDetailPadding),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      pet.name,
+                      style: const TextStyle(
+                        fontWeight: FontWeight.bold,
+                        fontSize: Sizes.normalFontSize,
+                      ),
                     ),
-                  ),
-                  const SizedBox(height: Sizes.spacer / 2),
-                  Row(
-                    children: [
-                      const Icon(
-                        Icons.cake,
-                        color: Colors.grey,
-                        size: Sizes.cakeIcon,
-                      ),
-                      const SizedBox(width: Sizes.spacer / 4),
-                      Text(
-                        '${pet.age} years old',
-                        style: const TextStyle(
-                          fontSize: Sizes.smallFontSize,
+                    const SizedBox(height: Sizes.spacer / 2),
+                    Row(
+                      children: [
+                        const Icon(
+                          Icons.cake,
                           color: Colors.grey,
+                          size: Sizes.cakeIcon,
                         ),
-                      ),
-                      const Spacer(),
-                      Text(
-                        'Breed: ${pet.breed} / Rs ${pet.price}',
-                        style: const TextStyle(
-                          fontSize: Sizes.smallFontSize,
-                          color: Colors.grey,
+                        const SizedBox(width: Sizes.spacer / 4),
+                        Text(
+                          '${pet.age} years old',
+                          style: const TextStyle(
+                            fontSize: Sizes.smallFontSize,
+                            color: Colors.grey,
+                          ),
                         ),
-                      ),
-                    ],
-                  ),
-                ],
+                        const Spacer(),
+                        Text(
+                          'Breed: ${pet.breed} / Rs ${pet.price}',
+                          style: const TextStyle(
+                            fontSize: Sizes.smallFontSize,
+                            color: Colors.grey,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
               ),
-            ),
-          ],
+            ],
+          ),
         ),
       ),
     );
