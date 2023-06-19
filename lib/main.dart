2e4a3f926b61fbb5ae1355f1dd479837341a3ea9
Abhisek Kumar Gupta <codewhispererisalive@gmail.com>
2023-06-19 23:29:51 +0530

    use provider

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,12 @@
 // main.dart
 import 'package:flutter/material.dart';
 import 'package:pa/screens/home_screen.dart';
+import 'package:pa/theme/styles.dart';
 import 'package:pa/widgets/pet_avatar.dart';
 import 'package:pa/screens/pet_details.dart';
+import 'package:provider/provider.dart';
 
+import 'models/theme_provider.dart';
 import 'widgets/card.dart';
 import 'data/pet_data.dart';
 
@@ -11,11 +14,43 @@ void main() {
   runApp(const MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   const MyApp({Key? key}) : super(key: key);
 
   @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  DarkThemeProvider themeChangeProvider = DarkThemeProvider();
+
+  @override
+  void initState() {
+    super.initState();
+    getCurrentAppTheme();
+  }
+
+  void getCurrentAppTheme() async {
+    themeChangeProvider.darkTheme =
+        await themeChangeProvider.darkThemePreference.getTheme();
+  }
+
+  @override
   Widget build(BuildContext context) {
+    return ChangeNotifierProvider(
+      create: (_) {
+        return themeChangeProvider;
+      },
+      child: Consumer<DarkThemeProvider>(
+        builder: (BuildContext context, value, Widget? child) {
+          return MaterialApp(
+            debugShowCheckedModeBanner: false,
+            theme: Styles.themeData(themeChangeProvider.darkTheme, context),
+            home: HomePage(),
+          );
+        },
+      ),
+    );
     return const MaterialApp(
       // Remove the debug banner
       debugShowCheckedModeBanner: false,
