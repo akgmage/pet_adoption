e947955247e404b30be78ce287a713d497335e96
Abhisek Kumar Gupta <codewhispererisalive@gmail.com>
2023-06-19 23:42:03 +0530

    add primaryWhite

--- lib/constants/color.dart
+++ lib/constants/color.dart
@@ -15,4 +15,20 @@ const MaterialColor primaryBlack = MaterialColor(
     900: Color(0xFF000000),
   },
 );
+const MaterialColor primaryWhite = MaterialColor(
+  whitePrimaryValue,
+  <int, Color>{
+    50: Color(0xFFFFFFFF),
+    100: Color(0xFFFFFFFF),
+    200: Color(0xFFFFFFFF),
+    300: Color(0xFFFFFFFF),
+    400: Color(0xFFFFFFFF),
+    500: Color(0xFFFFFFFF),
+    600: Color(0xFFFFFFFF),
+    700: Color(0xFFFFFFFF),
+    800: Color(0xFFFFFFFF),
+    900: Color(0xFFFFFFFF),
+  },
+);
 const int blackPrimaryValue = 0xFF000000;
+const int whitePrimaryValue = 0xFFFFFFFF;
