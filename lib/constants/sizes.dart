2127b1c1b14c0e316b4b33647f80b3be36439dde
Abhisek Kumar Gupta <codewhispererisalive@gmail.com>
2023-06-20 19:45:20 +0530

    remove unused size

--- lib/constants/sizes.dart
+++ lib/constants/sizes.dart
@@ -1,13 +1,8 @@
 class Sizes {
-  static const double size_8 = 8.0;
-  static const double size_12 = 12.0;
   static const double smallFontSize = 14.0;
   static const double normalFontSize = 18.0;
   static const double mediumFontSize = 20.0;
   static const double spacer = 16.0;
-  static const double size_20 = 20.0;
-  static const double size_24 = 24.0;
-  static const double size_100 = 100.0;
   static const double photoViewHeight = 300.0;
   static const double circleAvatarRadius = 50.0;
   static const double topFilterHeight = 100.0;
